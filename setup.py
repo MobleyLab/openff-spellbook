b02ee2ed3173f4b6e1ac26f910583774826c7def
Trevor Gokey <trevorgokey@gmail.com>
2020-07-28 16:09:23 -0700

    changes to push to pypi

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
-from setuptools import setup
+import setuptools
 import versioneer
 
 requirements = [
     # package requirements go here
 ]
 
-setup(
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='openff-spellbook',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
@@ -13,19 +16,25 @@ setup(
     license="MIT",
     author="Trevor Gokey",
     author_email='tgokey@uci.edu',
-    url='https://github.com/trevorgokey/openff-spellbook',
-    packages=['offsb',
-              'offsb/op',
-              'offsb/tools',
-              'offsb/search',
-              'offsb/qcarchive',
-              'offsb/rdutil',
-              'treedi'],
+    url='https://github.com/mobleylab/openff-spellbook',
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    packages=setuptools.find_packages(),
     
     install_requires=requirements,
     keywords='openff-spellbook',
     classifiers=[
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-    ]
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
+    # packages=['offsb',
+    #           'offsb/op',
+    #           'offsb/tools',
+    #           'offsb/search',
+    #           'offsb/qcarchive',
+    #           'offsb/rdutil',
+    #           'treedi'],
