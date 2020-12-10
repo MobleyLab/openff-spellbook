354949d2fcc4cc072576ca00f117ac08020dfccc
Trevor Gokey <trevorgokey@gmail.com>
2020-12-10 11:06:14 -0800

    bugfixes

--- offsb/qcarchive/__init__.py
+++ offsb/qcarchive/__init__.py
@@ -7,19 +7,22 @@ __version__ = get_versions()["version"]
 del get_versions
 
 
-def qcmol_to_xyz(qcmol, atom_map=None, fnm=None, fd=None, comment=""):
+def qcmol_to_xyz(
+    qcmol, atom_map=None, fnm=None, fd=None, comment="", precision="16.12f"
+):
     syms = qcmol.symbols
     xyz = qcmol.geometry
-
-    if atom_map:
-        idx = sorted(list(atom_map.keys()))
-        syms = [syms[atom_map[i] - 1] for i in idx]
-        xyz = [xyz[atom_map[i] - 1] for i in idx]
 
     if not hasattr(xyz[0], "__iter__"):
         xyz = np.reshape(xyz, (-1, 3))
 
-    xyzformat = "{:4s} {:10.6f} {:10.6f} {:10.6}\n"
+    if atom_map:
+        syms = [syms[atom_map[i]] for i in atom_map]
+        xyz = [xyz[atom_map[i]] for i in atom_map]
+
+    precision = str(precision)
+
+    xyzformat = " ".join(["{:4s}"] + list(["{:" + precision + "}"] * 3)) + '\n'
 
     header = [str(len(syms)) + "\n", str(comment) + "\n"]
     xyzstr = []
