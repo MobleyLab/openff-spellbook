8b0f18d889323b26f7193252873f17fe38d8d151
Trevor Gokey <trevorgokey@gmail.com>
2021-01-12 11:40:32 -0800

    add --cache option to ui

--- offsb/ui/qca/ff-label.py
+++ offsb/ui/qca/ff-label.py
@@ -12,6 +12,7 @@ if __name__ == "__main__":
     parser.add_argument('openff_name', type=str)
     parser.add_argument('--out_file_name', type=str)
     parser.add_argument('--datasets', type=str)
+    parser.add_argument('--cache', type=str)
 
     args = parser.parse_args()
 
@@ -19,7 +20,7 @@ if __name__ == "__main__":
     if args.datasets is not None:
         datasets = offsb.ui.qcasb.load_dataset_input(args.datasets)
 
-    obj = offsb.ui.qcasb.QCArchiveSpellBook(datasets=datasets)
+    obj = offsb.ui.qcasb.QCArchiveSpellBook(datasets=datasets, QCA=args.cache)
 
 
     out = args.out_file_name
