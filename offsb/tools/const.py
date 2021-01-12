33adfa47f6035ab88d3fcbd0bcdedaf7a71ef805
Trevor Gokey <trevorgokey@gmail.com>
2021-01-12 11:33:19 -0800

    kT and nm-related constants

--- offsb/tools/const.py
+++ offsb/tools/const.py
@@ -2,9 +2,20 @@
 from math import pi
 hartree2kcalmol = 627.5096080306
 kcalmol2hartree = 1.0/hartree2kcalmol
-bohr2angstrom = 0.52917721067
-angstrom2bohr = 1.0/bohr2angstrom
+cal2j = 4.184
+kjmol2hartree = kcalmol2hartree * cal2j
+hartree2kjmol = 1/kjmol2hartree
+
 angstrom2nm   = 0.1
+nm2angstrom   = 10.0
+
+bohr2angstrom = 0.52917721067
+bohr2nm = bohr2angstrom * angstrom2nm
+angstrom2bohr = 1.0/bohr2angstrom
+nm2bohr = 1.0/bohr2nm
+
+kT2kjmol = 2.479
+kT2kcalmol = 0.592
 
 radian2degree = pi / 180.
 
